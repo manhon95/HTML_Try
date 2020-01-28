e8f2eacbdd0b2773bc09473b6db3e99be9084e92
manhon95 <manhon95@gmail.com>
2020-01-28 23:28:50 +0800

    Added Log In/Out Function

--- DB_Conn_Try.py
+++ DB_Conn_Try.py
@@ -1,16 +1,35 @@
 import sqlite3
 
-def add_account(username):
+def add_account(user_id, user_pass):
+    try:
+        db = sqlite3.connect('DB_Try.db')
+        db.execute("insert into account('ac_id', 'ac_pass') values (:user_id, :user_pass)", {'user_id':user_id, 'user_pass':user_pass})
+        db.commit()
+        db.close()
+        print(user_id + ' added to DB')
+        return True
+    except:
+        db.close()
+        return False
+
+def edit_account(user_id, new_username):
     db = sqlite3.connect('DB_Try.db')
-    db.execute("insert into account('ac_name') values (:username)", {'username':username})
+    db.execute("update account set ac_name = :new_username where ac_id = :user_id", {'new_username':new_username, 'user_id':user_id})
     db.commit()
     db.close()
-    print(username + ' added to DB')
+    print(user_id + ' changed name to ' + new_username)
     return True
 
-def get_account_info(username):
+def password_match(user_id, user_pass):
     db = sqlite3.connect('DB_Try.db')
-    info = db.execute("select * from account where username = :username", {'username':username}).fetchall()
-    print(username + ' get from DB')
+    match_case_count = db.execute("select count(*) from account where ac_id = :user_id and ac_pass = :user_pass", {'user_id':user_id, 'user_pass':user_pass}).fetchall()[0][0]
+    db.commit()
+    db.close()
+    return (match_case_count > 0)
+
+def get_user_name(user_id):
+    db = sqlite3.connect('DB_Try.db')
+    info = db.execute("select ac_name from account where ac_id = :user_id", {'user_id':user_id}).fetchall()
+    print(user_id + ' get from DB')
     db.close()
     return True, info
--- Flask_Try.py
+++ Flask_Try.py
@@ -1,28 +1,92 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, make_response, session
 import DB_Conn_Try
 
 app = Flask(__name__)
+app.secret_key = 'fkdjsafjdkfdlkjfadskjfadskljdsfklj'
 
-#網頁執行/say_hello時，會導至index.html
-@app.route('/say_hello', methods=['GET'])
-def getdata():
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
     return render_template('index.html')
 
-#index.html按下submit時，會取得前端傳來的username，並回傳"Hello, World! "+name
-@app.route('/say_hello', methods=['POST'])
-def submit():
-    name = request.form.get('username')
-    if DB_Conn_Try.add_account(name):
-        return "Hello, " + name
+
+#------------------------------------------------------------------------------------------------
+#--------------------------------------Using cookies---------------------------------------------
+#------------------------------------------------------------------------------------------------
+@app.route('/user_cookies', methods=['GET', 'POST'])
+def user_cookies():
+    user_id = request.form.get('user_id')
+    user_pass = request.form.get('password')
+    if DB_Conn_Try.add_account(user_id, user_pass):
+        resp = make_response(render_template('user.html', user_id = user_id))
+        resp.set_cookie('user_id', user_id)
+        return resp
     else:
         return "Add account Fail"
 
-def get_ac_info():
-    return True
 
-@app.route('/shutdown', methods=['GET'])
-def shutdown():
-    return 'Server shutting down...'
+@app.route('/edit_cookies', methods=['POST'])
+def edit_user_cookies():
+    new_username = request.form.get('username')
+    print(new_username)
+    user_id = request.cookies.get('user_id')
+    print(user_id)
+    if DB_Conn_Try.edit_account(user_id, new_username):
+        return render_template('user.html', user_id = user_id)
+    else:
+        return "Edit account Fail"
+
+
+@app.route('/login_cookies', methods=['POST'])
+def login_cookies():
+    user_id = request.form.get('user_id')
+    user_pass = request.form.get('password')
+    if DB_Conn_Try.password_match(user_id, user_pass):
+        resp = make_response(render_template('user.html', user_id = user_id))
+        resp.set_cookie('user_id', user_id)
+        return resp
+    else:
+        return "Login Fail"
+
+#------------------------------------------------------------------------------------------------
+#--------------------------------------Using sessions---------------------------------------------
+#------------------------------------------------------------------------------------------------
+@app.route('/user_sessions', methods=['GET', 'POST'])
+def user_sessions():
+    user_id = request.form.get('user_id')
+    user_pass = request.form.get('password')
+    if DB_Conn_Try.add_account(user_id, user_pass):
+        session['user_id'] = user_id
+        return render_template('user.html', user_id = user_id)
+    else:
+        return "Add account Fail"
+
+
+@app.route('/edit_sessions', methods=['POST'])
+def edit_user_sessions():
+    user_id = session['user_id']
+    new_username = request.form.get('username')
+    print(new_username)
+    print(user_id)
+    if DB_Conn_Try.edit_account(user_id, new_username):
+        return render_template('user.html', user_id = user_id)
+    else:
+        return "Edit account Fail"
+
+
+@app.route('/login_sessions', methods=['POST'])
+def login_sessions():
+    user_id = request.form.get('user_id')
+    user_pass = request.form.get('password')
+    if DB_Conn_Try.password_match(user_id, user_pass):
+        session['user_id'] = user_id
+        return render_template('user.html', user_id = user_id)
+    else:
+        return "Login Fail"
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
